891d4c1c7d0c7125f4c54a1746196c6a93f13fd1
maxgfr <25312957+maxgfr@users.noreply.github.com>
2026-04-03 14:12:13 +0200

    chore: update github-helpers to v1.1.0

--- Formula/github-helpers.rb
+++ Formula/github-helpers.rb
@@ -1,9 +1,9 @@
 class GithubHelpers < Formula
   desc "GitHub maintenance toolkit: bulk unstar, org clone, and more"
   homepage "https://github.com/maxgfr/github-helpers"
-  url "https://github.com/maxgfr/github-helpers/archive/refs/tags/v1.0.0.tar.gz"
-  version "v1.0.0"
-  sha256 "4b50254eda6ad8dff74d746d3a14cdbf22c2aa4ca4821f2f54f2b0324ec09e96"
+  url "https://github.com/maxgfr/github-helpers/archive/refs/tags/v1.1.0.tar.gz"
+  version "v1.1.0"
+  sha256 "38d5b2d10f12daa7c78321f96e0b4dce30608458a36f0a6ad0d0d3bcd99770f5"
   license "MIT"
 
   depends_on "gh"
